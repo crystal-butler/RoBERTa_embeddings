b2031a6c696011a5870897f4985db6dfd46711dd
Crystal Butler <crystal.butler@arup.com>
2020-05-16 18:08:00 -0400

    add model import and setup

--- make_contextual_embeddings.py
+++ make_contextual_embeddings.py
@@ -1,4 +1,5 @@
 import argparse
+from transformers import RobertaTokenizer, RobertaModel, RobertaForMaskedLM, RobertaConfig
 
 parser = argparse.ArgumentParser()
 parser.add_argument('model_path', help='a directory containing the output from ' \
@@ -9,5 +10,10 @@ parser.add_argument('vocab_file', help='a newline-delimited list of vocabulary w
 parser.add_argument('output_file', help='path to the file where embeddings should be written', type=str)
 parser.add_argument('--count_file', help='optional path to a file where counts of the number ' \
     'of context sentences per vocabulary word should be written', default=None, type=str)
+args = parser.parse_args()
+
+tokenizer = RobertaTokenizer.from_pretrained(args.model_path)
+model = RobertaForMaskedLM.from_pretrained(args.model_path)
+model.eval()
 
 
