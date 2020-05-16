572aee5fea685fab86e1165566c35037a83e107c
Crystal Butler <crystal.butler@arup.com>
2020-05-16 18:26:56 -0400

    add torch import

--- make_contextual_embeddings.py
+++ make_contextual_embeddings.py
@@ -1,4 +1,5 @@
 import argparse
+import torch
 from transformers import RobertaTokenizer, RobertaModel, RobertaForMaskedLM, RobertaConfig
 import utils
 import distill_layers
@@ -23,59 +24,59 @@ FEATURE_COUNT = 768  # Change this value to 1024 for the large RoBERTa model.
 MAX_LINES = 2000  # Maximum number of context lines to average per vocabulary embedding.
 
 
-# Process vocabulary words in the outer loop.
-for v in vocab:
-    start = timer()
-    with open(context_file, 'r') as lines:
-        v_sum = torch.zeros([1, FEATURE_COUNT])
-        v_tokens = tokenize_text(v, tokenizer)
-        print_tokenized_text(v_tokens, tokenizer)
-        count_sentence = 0
-        count_tensor = 0
-        
-        # Process all lines in the context file in the inner loop.
-        for line in lines:
-            # Check for this vocab word in this line; if found, split the line into individual sentences.
-            if v in line.lower().split():
-                for sentence in line.split('.'):
-                    if v in sentence.lower():
-                        line = sentence
-                        count_sentence += 1
-                        print(f'\nInstance {count_sentence} of {tokenizer.decode(v_tokens[1:-1]).strip()}.')
-                        break  # We'll take the first instance of the word and discard the rest of the line.
-                # Split the new sentence-based line into tokens.
-                line_tokens = tokenize_text(line, tokenizer)               
-                # Get the indices of the line at which our vocabulary word tokens are located.
-                indices = get_vocab_indices(v_tokens, line_tokens, tokenizer)                             
+if __name__ == "__main__":
+    # Process vocabulary words in the outer loop.
+    for v in vocab:
+        with open(args.context_file, 'r') as lines:
+            v_sum = torch.zeros([1, FEATURE_COUNT])
+            v_tokens = tokenize_text(v, tokenizer)
+            print_tokenized_text(v_tokens, tokenizer)
+            count_sentence = 0
+            count_tensor = 0
+            
+            # Process all lines in the context file in the inner loop.
+            for line in lines:
+                # Check for this vocab word in this line; if found, split the line into individual sentences.
+                if v in line.lower().split():
+                    for sentence in line.split('.'):
+                        if v in sentence.lower():
+                            line = sentence
+                            count_sentence += 1
+                            print(f'\nInstance {count_sentence} of {tokenizer.decode(v_tokens[1:-1]).strip()}.')
+                            break  # We'll take the first instance of the word and discard the rest of the line.
+                    # Split the new sentence-based line into tokens.
+                    line_tokens = tokenize_text(line, tokenizer)               
+                    # Get the indices of the line at which our vocabulary word tokens are located.
+                    indices = get_vocab_indices(v_tokens, line_tokens, tokenizer)                             
 
-                # If the vocabulary word was found, process the containing line.
-                if indices:
-                    # Get the feature vectors for all tokens in the line/sentence.
-                    token_embeddings = create_token_embeddings(line_tokens)
-                    # Select a method for extracting specific layers of the model.
-                    token_vecs_layer = get_layer_token_vecs(token_embeddings, 12)
-                    # Sum the individual token contextual embeddings for the whole vocab word, for this line.
-                    tensor_layer = torch.zeros([1, FEATURE_COUNT])
-                    for i in range(len(indices)):
-                        preview_token_embedding(v_tokens, token_vecs_layer, i, indices, tokenizer)
-                        tensor_layer += token_vecs_layer[indices[i]]
-                    # If our vocab word is broken into more than one token, we need to get the mean of the token embeddings.
-                    tensor_layer /= len(indices)
+                    # If the vocabulary word was found, process the containing line.
+                    if indices:
+                        # Get the feature vectors for all tokens in the line/sentence.
+                        token_embeddings = create_token_embeddings(line_tokens)
+                        # Select a method for extracting specific layers of the model.
+                        token_vecs_layer = get_layer_token_vecs(token_embeddings, 12)
+                        # Sum the individual token contextual embeddings for the whole vocab word, for this line.
+                        tensor_layer = torch.zeros([1, FEATURE_COUNT])
+                        for i in range(len(indices)):
+                            preview_token_embedding(v_tokens, token_vecs_layer, i, indices, tokenizer)
+                            tensor_layer += token_vecs_layer[indices[i]]
+                        # If our vocab word is broken into more than one token, we need to get the mean of the token embeddings.
+                        tensor_layer /= len(indices)
 
-                    # Add the embedding distilled from this line to the sum of embeddings for all lines.
-                    v_sum += tensor_layer
-                    count_tensor += 1
-                    print(f'Grand sum of {count_tensor} tensor sets is: {v_sum[0][:5].tolist()}')
+                        # Add the embedding distilled from this line to the sum of embeddings for all lines.
+                        v_sum += tensor_layer
+                        count_tensor += 1
+                        print(f'Grand sum of {count_tensor} tensor sets is: {v_sum[0][:5].tolist()}')
 
-            # Stop processing lines once we've found 2000 instances of our vocab word.
-            if count_tensor >= MAX_LINES:
-                break
-        
-        # We're done processing all lines of 512 tokens or less containing our vocab word.
-        # Get the mean embedding for the word.
-        v_mean = v_sum / count_tensor
-        print(f'Mean of {count_tensor} tensors is: {v_mean[0][:5]} (first 5 of {len(v_mean[0])} features in tensor)')
-        write_embedding(output_file, v, v_mean)
-        write_line_count(count_file, v, count_tensor)
-    end = timer()
-    print(f'Run time for {v} was {end - start} seconds.')
+                # Stop processing lines once we've found 2000 instances of our vocab word.
+                if count_tensor >= MAX_LINES:
+                    break
+            
+            # We're done processing all lines of 512 tokens or less containing our vocab word.
+            # Get the mean embedding for the word.
+            v_mean = v_sum / count_tensor
+            print(f'Mean of {count_tensor} tensors is: {v_mean[0][:5]} (first 5 of {len(v_mean[0])} features in tensor)')
+            write_embedding(output_file, v, v_mean)
+            write_line_count(count_file, v, count_tensor)
+        end = timer()
+        print(f'Run time for {v} was {end - start} seconds.')
