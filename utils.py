43707dcd0324800d76050c41de3aed1d4b27934f
Crystal Butler <crystal.butler@arup.com>
2020-05-16 16:47:37 -0400

    formatting

--- utils.py
+++ utils.py
@@ -43,6 +43,7 @@ def create_token_embeddings(tokenized_text):
         print(f'Size of token embeddings is {token_embeddings.size()}')
         return token_embeddings
 
+
 def preview_token_embedding(tokenized_text, layer, index, index_list, tokenizer):
     """Print the first 5 feature values from a model layer for tokens at specific line indices."""
     v_index = i % len(tokenized_text[1:-1])
