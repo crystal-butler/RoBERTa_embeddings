1f47f23849449fd216b880c300d17e7b823c7aae
Crystal Butler <crystal.butler@arup.com>
2020-05-16 17:21:13 -0400

    add get_layer_token_vecs

--- distill_layers.py
+++ distill_layers.py
@@ -11,3 +11,18 @@ def sum_last_four_token_vecs(token_embeddings):
 
     print ('Shape of summed layers is: %d x %d' % (len(token_vecs_sum_last_four), len(token_vecs_sum_last_four[0])))
     return token_vecs_sum_last_four
+
+
+def get_layer_token_vecs(token_embeddings, layer_number):
+    """Return a single layer of the model."""
+    token_vecs_layer = []
+
+    # For each token in the sentence...
+    for token in token_embeddings:
+        layer_vec = token[layer_number]
+
+        # Use `layer_vec` to represent `token`.
+        token_vecs_layer.append(layer_vec)
+
+    print ('Shape of summed layers is: %d x %d' % (len(token_vecs_layer), len(token_vecs_layer[0])))
+    return token_vecs_layer
