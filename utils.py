174d8a67551fd36b1f9ec40973cdf76d162e77d7
Crystal Butler <crystal.butler@arup.com>
2020-05-16 16:40:54 -0400

    add tokenize_text

--- utils.py
+++ utils.py
@@ -4,3 +4,10 @@ def make_vocab(vocab_file):
     with open(vocab_file, 'r') as v:
         vocab = v.read().splitlines()
     return vocab
+
+
+def tokenize_text(text, tokenizer):
+    """Break the input text into tokens the model can use, and return them.
+    Use max_length to avoid overflowing the maximum sequence length for the model."""
+    tokenized_text = tokenizer.encode(text, add_special_tokens=True, max_length=512)
+    return tokenized_text
