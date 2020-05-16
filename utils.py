3b7972439fc5a365de4a24a551d20d61bd8cc722
Crystal Butler <crystal.butler@arup.com>
2020-05-16 16:42:07 -0400

    add print_tokenized_text

--- utils.py
+++ utils.py
@@ -11,3 +11,12 @@ def tokenize_text(text, tokenizer):
     Use max_length to avoid overflowing the maximum sequence length for the model."""
     tokenized_text = tokenizer.encode(text, add_special_tokens=True, max_length=512)
     return tokenized_text
+
+
+def print_tokenized_text(tokens, tokenizer):
+    """Print the number of tokens in some tokenized text, not counting the leading and trailing separators.
+    Print each token without any leading or trailing whitespace."""
+    print(f'\nThere are {len(tokens) - 2} tokens in tokenized text:')
+    for t in tokens[1:-1]:
+        print(tokenizer.decode(t).strip())
+
