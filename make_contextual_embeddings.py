23a7f7692d542246197a0e3aa79264a6e82a8b3a
Crystal Butler <crystal.butler@arup.com>
2020-05-16 17:59:45 -0400

    add argument parsing

---
+++ make_contextual_embeddings.py
@@ -0,0 +1,13 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('model_path', help='a directory containing the output from ' \
+    'fine-tuning a huggingface RoBERTa model with hidden layers saved', type=str)
+parser.add_argument('context_file', help='a text file to use as context to create embeddings', type=str)
+parser.add_argument('vocab_file', help='a newline-delimited list of vocabulary words for which ' \
+    'to generate embeddings', type=str)
+parser.add_argument('output_file', help='path to the file where embeddings should be written', type=str)
+parser.add_argument('--count_file', help='optional path to a file where counts of the number ' \
+    'of context sentences per vocabulary word should be written', default=None, type=str)
+
+
